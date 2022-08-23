36159dd5e7c0eb54d4ed874a19f9047b67ba23c0
Roland Bapst <roland.bapst@tx.group>
2022-08-23 12:18:04 +0200

    add untracked files

--- test-variables/dummy.tf
+++ test-variables/dummy.tf
@@ -1,5 +1,5 @@
 locals {
-  domain_name = contains(keys(var.domain_infos), var.deployment_env) ? var.domain_infos.var.deployment_env.domain_name : ""
+  domain_name  = contains(keys(var.domain_infos), var.deployment_env) ? var.domain_infos.var.deployment_env.domain_name : ""
   release_name = "toto"
   service_name = local.release_name
 }
