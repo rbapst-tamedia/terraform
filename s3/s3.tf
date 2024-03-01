6e2454684b2abcfc1fff76d95422a4c035c6f607
Roland Bapst <roland.bapst@tx.group>
2024-03-01 10:53:56 +0100

    test(s3): test of the aws s3 module

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,27 +1,36 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "rba-test-livecycle-bucket"
+variable "fuw_exchange_bucket_prefix" {
+  description = "Prefix of S3 bucket name to allow exchange of data between user and wordpress legacy fuw website. Set to empty to not create it. \"-AWS_ACCOUNT_ID\" will be append"
+  type        = string
+  default     = "test-rba"
+}
+
+locals {
+  deploy_fuw_exchange_bucket = var.fuw_exchange_bucket_prefix != "" ? true : false
+}
+
+# S3 Bucket
+module "fuw_exchange_bucket" {
+  count = local.deploy_fuw_exchange_bucket ? 1 : 0
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.1.0"
+
+  # It's a POC, reason of the "-poc" at the end. Should be removed if it's accepted
+  bucket = "${var.fuw_exchange_bucket_prefix}-${data.aws_caller_identity.current.account_id}-poc"
   acl    = "private"
-  tags = {
-    "owner" = "roland bapst"
-    "file"  = "https://github.com/rbapst-tamedia/terraform/s3.tf"
-  }
-  force_destroy = false
 
-  lifecycle_rule {
-    id      = "log"
-    enabled = true
-    prefix  = "3_days/"
-    expiration {
-      days = 3
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
     }
   }
 
-  lifecycle_rule {
-    id      = "tmp"
-    prefix  = "tmp/"
+  versioning = {
     enabled = true
-    expiration {
-      days = 1
-    }
   }
 }
